72fbad72029798134bcbf747a8c853e34463536b
chenxianglin <woailuozaizhe@sina.cn>
2017-04-05 15:27:04 +0800

    add base

---
+++ app/src/main/java/ps/cxl/com/pslibrary/base/BaseActivity.java
@@ -0,0 +1,25 @@
+package ps.cxl.com.pslibrary.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created : chenxianglin on 17/4/5.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}

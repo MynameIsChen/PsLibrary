9ae93af583d8dbfed810edf6635a0f8683e81090
chenxianglin <woailuozaizhe@sina.cn>
2017-04-05 16:25:14 +0800

    setting

--- core/src/main/java/ps/cxl/com/core/bean/BaseBean.java
+++ core/src/main/java/ps/cxl/com/core/bean/BaseBean.java
@@ -1,9 +1,11 @@
 package ps.cxl.com.core.bean;
 
+import java.io.Serializable;
+
 /**
  * Created : chenxianglin on 17/4/5.
  */
 
-public interface BaseBean {
+public interface BaseBean extends Serializable{
 
 }
